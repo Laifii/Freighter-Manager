4156ad98a17a4b1a0a7318b00a77312e1c5aecc7
Laifii <lanamcrowe@gmail.com>
2025-06-13 03:40:52 +0100

    Added custom font and added nameplate above stations

--- Station.gd
+++ Station.gd
@@ -5,6 +5,7 @@ extends Node2D
 @export_enum("Local", "Connector", "Large", "Hub") var stationSize
 @export_enum("None", "Player", "United Rail", "Birmingham Express", "Scottish Rail", "London Freighters") var stationOwner
 @export var connections: Array
+@onready var nameplate = $Sprite2D/Nameplate
 
 var stationValue # value = 500000 * (connections.size() / 2) * company multiplier
 var notInContract = true
@@ -19,4 +20,17 @@ var contract = {
 }
 
 func _ready():
+	if Settings.stationNamesAlwaysVisible: nameplate.visible = true
 	StationManager.stations.append(self)
+	nameplate.text = stationName
+
+
+func _on_area_2d_mouse_entered():
+	if Settings.stationNamesAlwaysVisible: return
+	nameplate.visible = true
+	print(nameplate.text)
+
+
+func _on_area_2d_mouse_exited():
+	if Settings.stationNamesAlwaysVisible: return
+	nameplate.visible = false
